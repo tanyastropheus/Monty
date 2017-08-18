1c4c8b6e82463413b6e20febf362b5c8d24a9c0d
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-18 23:16:05 +0000

    added the op_functions prototypes

--- monty.h
+++ monty.h
@@ -62,5 +62,9 @@ stack_t *create_new_node(unsigned int line_number);
 void op_push(stack_t **stack, unsigned int line_number);
 void op_pall(stack_t **stack, unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 #endif /* MONTY_H */
