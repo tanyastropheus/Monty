85e79230b904583c9f42dd25b8b64b0a70a40aaf
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-19 17:43:15 +0000

    added sub(), div(), mul() prototypes

--- monty.h
+++ monty.h
@@ -66,5 +66,8 @@ void op_pop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 void op_add(stack_t **stack, unsigned int line_number);
 void op_nop(stack_t **stack, unsigned int line_number);
+void op_sub(stack_t **stack, unsigned int line_number);
+void op_div(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 #endif /* MONTY_H */
