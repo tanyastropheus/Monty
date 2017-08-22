a5027e41fc7c259857507d918794b1ead55903b0
Tanya Kryukova <ttkryukova@gmail.com>
2017-08-22 02:56:58 +0000

    deleted extra empty line

--- create_new_node.c
+++ create_new_node.c
@@ -16,9 +16,17 @@ stack_t *create_new_node(unsigned int line_number)
 	if (!new)
 		err_message(ERR_MALLOC, NULL, 0);
 	operand = strtok(NULL, "\n\r\t ");
+	if (!operand) /* if not operand is given */
+	{
+		free(new);
+		err_message(ERR_INT, NULL, line_number);
+	}
 	op_num = strtol(operand, &end, 10);
 	if (*end) /* operand is not a num & can't be converted */
+	{
+		free(new);
 		err_message(ERR_INT, NULL, line_number);
+	}
 	new->n = op_num;
 	return (new);
 }
--- monty.h
+++ monty.h
@@ -1,6 +1,5 @@
 #ifndef MONTY_H
 #define MONTY_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
